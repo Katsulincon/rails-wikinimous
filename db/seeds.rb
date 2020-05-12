481fdf82208c0330df5a2e4c5e7b298202c79672
Munekatsu Furugen <rinconlincon@gmail.com>
2020-05-12 15:42:12 +0900

    Created seeds and initialized controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Movie.quote
+  article = Article.new(title: title, content: content)
+  article.save
+end
